4c7aa7efec64b19bd59fd6b50dcff2f591d36ad4
Yaima Fernández Segredo <yfsegredo@gmail.com>
2022-02-09 02:48:14 -0500

    Add plain_webhook services

---
+++ spec/code_samples/NodeJS/setup@plain_webhook/post.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv')
+const axios = require('axios');
+const https = require('https');
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
+dotenv.config();
+
+axios.defaults.baseURL = process.env['BASE_URL'] || 'https://cenit.io/api/v2/';
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.headers.common['X-Tenant-Access-Key'] = process.env['X_TENANT_ACCESS_KEY']
+axios.defaults.headers.common['X-Tenant-Access-Token'] = process.env['X_TENANT_ACCESS_TOKEN']
+
+const request = axios({
+  method: 'POST',
+  url: 'setup/plain_webhook',
+  httpsAgent: agent,
+  data: {
+    namespace: "Test",
+    name: "webhook_test_01",
+    method: "get",
+    path: "api/v2/test_01",
+  }
+});
+
+request.then((response) => {
+  // Do something with the response
+  console.log(JSON.stringify(response.data, null, 2));
+}).catch((error) => {
+  // Do something with the error
+  console.error(JSON.stringify(error.response.data, null, 2));
+});
+
