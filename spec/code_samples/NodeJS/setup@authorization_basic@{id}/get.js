d5e19df0212c9e2266cc0a988cd5824316994ed2
Yaima Fernández Segredo <yfsegredo@gmail.com>
2022-02-11 12:24:52 -0500

    Add code samples for basic-authorization management

---
+++ spec/code_samples/NodeJS/setup@authorization_basic@{id}/get.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv')
+const axios = require('axios');
+
+dotenv.config();
+
+axios.defaults.baseURL = process.env['BASE_URL'] || 'https://cenit.io/api/v2/';
+axios.defaults.headers.common['X-Tenant-Access-Key'] = process.env['X_TENANT_ACCESS_KEY']
+axios.defaults.headers.common['X-Tenant-Access-Token'] = process.env['X_TENANT_ACCESS_TOKEN']
+
+const item_id = '62069b095a5a2330d0038cdc'
+
+const request = axios({
+  method: 'GET',
+  url: `setup/basic_authorization/${item_id}`,
+});
+
+request.then((response) => {
+  // Do something with the response
+  console.log(JSON.stringify(response.data, null, 2));
+}).catch((error) => {
+  // Do something with the error
+  console.error(JSON.stringify(error.response.data, null, 2));
+});
---
+++ spec/code_samples/NodeJS/setup@authorization_basic@{id}/post.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv')
+const axios = require('axios');
+
+dotenv.config();
+
+axios.defaults.baseURL = process.env['BASE_URL'] || 'https://cenit.io/api/v2/';
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.headers.common['X-Tenant-Access-Key'] = process.env['X_TENANT_ACCESS_KEY']
+axios.defaults.headers.common['X-Tenant-Access-Token'] = process.env['X_TENANT_ACCESS_TOKEN']
+
+const item_id = '62069b095a5a2330d0038cdc'
+
+let request = axios({
+  method: 'POST',
+  url: `setup/basic_authorization/${item_id}`,
+  data: {
+    username: "test_auth_basic@api_v2"
+  }
+});
+
+request.then((response) => {
+  // Do something with the response
+  console.log(JSON.stringify(response.data, null, 2));
+}).catch((error) => {
+  // Do something with the error
+  console.error(JSON.stringify(error.response.data, null, 2));
+});
