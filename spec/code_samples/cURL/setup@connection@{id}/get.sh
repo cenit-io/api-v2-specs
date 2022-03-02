c82c5ba3baa936e0e1919bbbc332f3657a282c8e
Yaima Fernández Segredo <yfsegredo@gmail.com>
2022-03-02 18:48:27 -0500

    Update code samples for connection management

--- spec/code_samples/cURL/setup@connection@{id}/get.sh
+++ spec/code_samples/cURL/setup@connection@{id}/get.sh
@@ -2,7 +2,7 @@
 
 base_url=${BASE_URL:="https://cenit.io/api/v2"}
 path="setup/connection"
-item_id="59d5502825d9852797009590"
+item_id="62070ef05a5a23307d038a63"
 
 curl -G "${base_url}/${path}/${item_id}" \
   -H "X-Tenant-Access-Key: ${X_TENANT_ACCESS_KEY}" \
