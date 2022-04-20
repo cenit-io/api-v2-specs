ad731cdeacad1a0751a85de31cb38b591fbb5da2
Yaima Fernández Segredo <yfsegredo@gmail.com>
2022-04-20 19:51:48 -0400

    Update ruby_template examples

---
+++ spec/code_samples/cURL/setup@ruby_template/post.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+base_url=${BASE_URL:="https://cenit.io/api/v2"}
+path="setup/ruby_template"
+
+read -r -d '' data <<- EOM
+  {
+      "namespace": "Test",
+      "name": "template_test_01",
+      "mime_type": "application/json",
+      "file_extension": "json",
+      "bulk_source": false,
+      "source_data_type": {
+        "_reference": true,
+        "namespace": "Test",
+        "name": "Person"
+      },
+      "code": "source.to_json"
+  }
+EOM
+
+curl "${base_url}/${path}" \
+  -X "POST" \
+  -H "X-Tenant-Access-Key: ${X_TENANT_ACCESS_KEY}" \
+  -H "X-Tenant-Access-Token: ${X_TENANT_ACCESS_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d "${data}"
