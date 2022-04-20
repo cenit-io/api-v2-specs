80f45d53e5a8e754430642d6f52de7867e0e06b1
Yaima Fernández Segredo <yfsegredo@gmail.com>
2022-04-20 19:37:14 -0400

    Update ruby_converter examples

---
+++ spec/code_samples/cURL/setup@ruby_converter@{id}/get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+base_url=${BASE_URL:="https://cenit.io/api/v2"}
+path="setup/ruby_converter"
+item_id="6203c5dd5a5a233de3027b96"
+
+curl -G "${base_url}/${path}/${item_id}" \
+  -H "X-Tenant-Access-Key: ${X_TENANT_ACCESS_KEY}" \
+  -H "X-Tenant-Access-Token: ${X_TENANT_ACCESS_TOKEN}" \
+  -d embedding=snippet
---
+++ spec/code_samples/cURL/setup@ruby_converter@{id}/post.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+base_url=${BASE_URL:="https://cenit.io/api/v2"}
+path="setup/ruby_converter"
+item_id="6203c5dd5a5a233de3027b96"
+
+read -r -d '' data <<- EOM
+  {
+       "source_handler": true,
+       "code": "sources.each {|s| target_data_type.create_from_json!(name: s.name.downcase, primary_field: %i[name]) }"
+  }
+EOM
+
+curl "${base_url}/${path}/${item_id}" \
+  -X "POST" \
+  -H "X-Tenant-Access-Key: ${X_TENANT_ACCESS_KEY}" \
+  -H "X-Tenant-Access-Token: ${X_TENANT_ACCESS_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d "${data}"
